bd4dbd32bc9bca4c7808d7bcb286a6e505938944
Dipen <dipendraboyaju222@gmail.com>
2024-05-19 13:34:52 +0545

    change

--- controller/productController.js
+++ controller/productController.js
@@ -4,14 +4,10 @@ import Product from '../models/Product.js'
 export const getAllProduct = async (req, res) => {
   try {
     const products = await Product.find();
-    if (products.length === 0) {
-      return res.status(404).json({ status: 'error', message: 'No products found' });
-    }
-    return res.status(200).json({ status: 'success', data: products });
-    // return res.status(200).json({
-    //   status: 'success',
-    //   data: products
-    // });
+    return res.status(200).json({
+      status: 'success',
+      data: products
+    })
   } catch (err) {
     return res.status(404).json({
       status: 'error',
